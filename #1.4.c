d4d549af6729cd34a9a585c1eb907c2655bac2bb
Prazwol Stark <prazwolstark@gmail.com>
2022-06-08 11:40:56 +0545

    54

--- #1.4.c
+++ #1.4.c
@@ -9,9 +9,10 @@ struct std
 };
 int main()
 {
-    FILE *SR
+    FILE *SR;
     struct std s[10];
     int n;
+    int j=0;
     printf("Enter the number of student's data you wanna enter?\n");
     scanf("%d",&n);
     SR = fopen("Student.txt",'a+');
@@ -22,17 +23,23 @@ int main()
         fprintf(SR,"%s",s[i].name);
         printf("\nRoll No.:\n>");
         scanf("%d",&s[i].rollno);
-        fprintf(SR,"%d",&s[i].rollno);
+        fprintf(SR,"%d",s[i].rollno);
         printf("\nAddress:\n>");
         scanf("%s",s[i].address);
         fprintf(SR,"%s",s[i].address);
         printf("\nPhone No.:\n>");
         scanf("%d",&s[i].phone_number);
-        fprintf(SR,"%d",&s[i].phone_number);
+        fprintf(SR,"%d",s[i].phone_number);
     }
     fclose(SR);
-    SR = fopen("Student.txt",'r');
-    while(fscanf())
+    FILE *PR;
+    PR = fopen("Student.txt",'r');
+    printf("\nName\t\tRoll NO.\tAdress\t\tPhone No.\n>");
+    while(fscanf(PR,"%s%d%s%d",s[j].name,s[j].rollno,s[j].address,s[j].phone_number)!=EOF)
+    {
+        printf("%s\t%d\t%s\t%d",s[j].name,s[j].rollno,s[j].address,s[j].phone_number);
+        j+=1;
+    }
+    fclose(PR);
     return 0;
-
 }
