2872605af2d8f86d60ddc303aa9d2a5ed66fabae
Prazwol Stark <prazwolstark@gmail.com>
2022-06-08 11:31:23 +0545

    6

--- #1.4.c
+++ #1.4.c
@@ -1,6 +1,38 @@
 #include<stdio.h>
+#include<stdlib.h>
+struct std
+{
+    char name[50];
+    int rollno;
+    char address[100];
+    long int phone_number;
+};
 int main()
 {
-    
+    FILE *SR
+    struct std s[10];
+    int n;
+    printf("Enter the number of student's data you wanna enter?\n");
+    scanf("%d",&n);
+    SR = fopen("Student.txt",'a+');
+    for(int i=0;i<n;i++)
+    {
+        printf("\nName:\n>");
+        scanf("%s",s[i].name);
+        fprintf(SR,"%s",s[i].name);
+        printf("\nRoll No.:\n>");
+        scanf("%d",&s[i].rollno);
+        fprintf(SR,"%d",&s[i].rollno);
+        printf("\nAddress:\n>");
+        scanf("%s",s[i].address);
+        fprintf(SR,"%s",s[i].address);
+        printf("\nPhone No.:\n>");
+        scanf("%d",&s[i].phone_number);
+        fprintf(SR,"%d",&s[i].phone_number);
+    }
+    fclose(SR);
+    SR = fopen("Student.txt",'r');
+    while(fscanf())
     return 0;
+
 }
--- Test.c
+++ Test.c
@@ -1,10 +1,18 @@
 #include<stdio.h>
 int main()
 {
-    int set[3]={1,2,3};
+    /*int set[3]={1,2,3};
     int *att;
     att = set;
     printf("%d\n",set);
     printf("%d\n",*att);
-    printf("%d",*(att+1));
+    printf("%d",*(att+1));*/
+    FILE *pr;
+    char name[50];
+    pr = fopen("HelloWorld.txt","a+");
+    fprintf(pr,"Hello World");
+    scanf("%s",name);
+    fprintf(pr,"%s",name);
+    fclose(pr);
+    return 0;
 }
